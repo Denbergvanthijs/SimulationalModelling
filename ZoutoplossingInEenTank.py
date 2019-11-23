c3ebbbfe95d36b81d96c7c4571f478d8250cdcee
denbergvanthijs <denbergvanthijs@gmail.com>
2019-11-23 15:34:36 +0100

    Zoutoplossing in een tank gemaakt, python 3.8 is benodigd voor de walrus operator

---
+++ ZoutoplossingInEenTank.py
@@ -0,0 +1,27 @@
+import matplotlib.pylab as plt
+import numpy as np
+
+
+def tank(ts=120, liters=1000, litersIn=6, litersUit=6, concGroei=0.1):
+    c = 0
+    conc = [c := c + (litersIn * concGroei / liters) - (litersUit / liters * c) for _ in range(ts + 1)]
+    return conc, np.arange(ts + 1)
+
+
+def plotMe(ts, liters, litersIn, litersUit, concGroei):
+    conc, tsArr = tank(ts=ts, liters=liters, litersIn=litersIn, litersUit=litersUit, concGroei=concGroei)
+    plt.plot(tsArr, conc, label=f"In={litersIn} Uit={litersUit} Groei={concGroei}")
+
+    plt.xlim(0, ts)
+    plt.ylim(bottom=0)
+    plt.title(f"Concentratie in kg/L in tank van {liters} liters")
+    plt.xlabel("Aantal stappen")
+    plt.ylabel("Concentratie in kg/L")
+    plt.legend()
+    plt.tight_layout()
+
+
+plotMe(1200, 1000, 6, 5, 0.1)
+plotMe(1200, 1000, 6, 6, 0.1)
+
+plt.show()
